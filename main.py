8a57c5356dc00308b8046dd08fc8d12ebe45d781
AntonTeterin <74729161+AntonTeterin@users.noreply.github.com>
2021-02-08 19:26:28 +0300

    №3

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class Window(QWidget):
         self.image = QLabel()
         self.mainlayout.addWidget(self.image)
 
-        self.toponym_longitude = '10'
-        self.toponym_lattitude = '50'
+        self.longitude = 10
+        self.lattitude = 50
         self.delta = 20
         self.mode = 'map'
         self.new_map = True
@@ -37,9 +37,10 @@ class Window(QWidget):
             map_url = 'http://static-maps.yandex.ru/1.x/'
             map_params = {
             'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-            'll': ','.join([self.toponym_longitude, self.toponym_lattitude]),
+            'll': ','.join([str(self.longitude), str(self.lattitude)]),
             'spn': ','.join([str(self.delta), str(self.delta)]),
-            'l': self.mode
+            'l': self.mode,
+            'size': '450,450'
             }
             response = requests.get(map_url, params=map_params)
             self.set_image(BytesIO(response.content))
@@ -51,6 +52,22 @@ class Window(QWidget):
         if event.key() == Qt.Key_PageDown:
             if self.delta >= 0.01:
                 self.delta = self.delta / 2
+        if event.key() == Qt.Key_Right:
+            self.longitude += self.delta * 2
+            if self.longitude >= 180:
+                self.longitude -= 360
+        if event.key() == Qt.Key_Left:
+            self.longitude -= self.delta * 2
+            if self.longitude < -180:
+                self.longitude += 360
+        if event.key() == Qt.Key_Up:
+            self.lattitude += self.delta * 2
+            if self.lattitude + self.delta > 90:
+                self.lattitude = 90 - self.delta
+        if event.key() == Qt.Key_Down:
+            self.lattitude -= self.delta * 2
+            if self.lattitude - self.delta < -90:
+                self.lattitude = -90 + self.delta
         self.new_map = True
         self.update()
 
