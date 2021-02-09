69ad3d4f65cedce4fe89177565b791d069a28ead
AntonTeterin <74729161+AntonTeterin@users.noreply.github.com>
2021-02-09 20:59:11 +0300

    №5, 6

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class Window(QWidget):
         self.layout1.addWidget(self.input_edit)
 
         self.search_btn = QPushButton('Искать')
+        self.search_btn.clicked.connect(self.search)
         self.layout1.addWidget(self.search_btn)
 
         self.mainlayout.addLayout(self.layout1)
@@ -46,23 +47,31 @@ class Window(QWidget):
         self.lattitude = 50
         self.delta = 20
         self.mode = 'map'
+        self.is_pt = False
         self.new_map = True
         self.update()
 
     def search(self):
-        search_url = "https://search-maps.yandex.ru/v1/"
-        api_key = "..."
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": self.input_edit.text(),
+            "format": "json"}
 
-        address_ll = "37.588392,55.734036"
-
-        search_params = {
-            "apikey": api_key,
-            "text": "аптека",
-            "lang": "ru_RU",
-            "ll": address_ll,
-            "type": "biz"
-        }
-        response = requests.get(search_url, params=search_params)
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        response = response.json()["response"]["GeoObjectCollection"][
+            "featureMember"]
+        if not response:
+            self.input_edit.setText('Ничего не найдено')
+            return
+        toponym = response[0]["GeoObject"]
+        res = [float(x) for x in self.get_coordinates(toponym)]
+        self.longitude, self.lattitude, self.delta = res
+        self.is_pt = True
+        self.pt_x = self.longitude
+        self.pt_y = self.lattitude
+        self.new_map = True
+        self.update()
 
     def set_mode(self):
         if self.radiobtn1.isChecked():
@@ -79,17 +88,27 @@ class Window(QWidget):
         image.loadFromData(img.getvalue())
         self.image.setPixmap(image)
 
+    def get_coordinates(self, toponym):
+        x, y = toponym["Point"]["pos"].split()
+        left, down = toponym["boundedBy"]["Envelope"]["lowerCorner"].split()
+        right, up = toponym["boundedBy"]["Envelope"]["upperCorner"].split()
+        delta = str(abs(float(right) - float(left)))
+        return x, y, delta
+
     def paintEvent(self, event):
         if self.new_map:
             self.new_map = False
             map_url = 'http://static-maps.yandex.ru/1.x/'
             map_params = {
-            'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-            'll': ','.join([str(self.longitude), str(self.lattitude)]),
-            'spn': ','.join([str(self.delta), str(self.delta)]),
-            'l': self.mode,
-            'size': '450,450'
+                'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+                'll': ','.join([str(self.longitude), str(self.lattitude)]),
+                'spn': ','.join([str(self.delta), str(self.delta)]),
+                'l': self.mode,
+                'size': '450,450'
             }
+            if self.is_pt:
+                map_params['pt'] = ','.join(
+                    [str(self.pt_x), str(self.pt_y), 'pm2grl'])
             response = requests.get(map_url, params=map_params)
             self.set_image(BytesIO(response.content))
 
@@ -119,7 +138,7 @@ class Window(QWidget):
         self.new_map = True
         self.update()
 
-            
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Window()
